64d5348ff46aa8fd84d33483b16c85818273ea05
Ilya Shcherbinin <m0n9oose@gmail.com>
2020-02-11 12:32:51 +0100

    Add CrushPics::Webhook helper class

--- lib/crush_pics.rb
+++ lib/crush_pics.rb
@@ -7,6 +7,7 @@ require 'crush_pics/exceptions'
 require 'crush_pics/configuration'
 require 'crush_pics/client'
 require 'crush_pics/response'
+require 'crush_pics/webhook'
 
 module CrushPics
   def self.configuration
---
+++ lib/crush_pics/webhook.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module CrushPics
+  class Webhook
+    attr_reader :payload, :event
+
+    def initialize(payload)
+      @event = payload.delete('event').inquiry
+      @quota_usage = payload.delete('quota_usage')
+      @payload = payload
+    end
+
+    def original_size_image
+      styles['original']
+    end
+
+    def image_id
+      payload['image_id']
+    end
+
+    def webhook_version
+      payload['id']
+    end
+
+    def styles
+      @styles ||= payload['optimized_images'].map { |img| [img['style'], img.slice('size', 'link', 'id', 'expire_at')] }.to_h
+    end
+  end
+end
